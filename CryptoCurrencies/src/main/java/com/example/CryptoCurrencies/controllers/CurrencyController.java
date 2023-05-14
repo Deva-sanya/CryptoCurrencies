610f7e2cdc081fdba95ab9d6eb3a2f767bf619b6
Alexandra Lebedzeva <evtuhova15vek44@gmail.com>
2023-05-14 13:22:42 +0300

    add allEnum currency method

--- CryptoCurrencies/src/main/java/com/example/CryptoCurrencies/controllers/CurrencyController.java
+++ CryptoCurrencies/src/main/java/com/example/CryptoCurrencies/controllers/CurrencyController.java
@@ -3,7 +3,6 @@ package com.example.CryptoCurrencies.controllers;
 import com.example.CryptoCurrencies.models.Currency;
 import com.example.CryptoCurrencies.services.CurrencyService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
